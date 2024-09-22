60627b0656ea8431f6f9c4245e42b10daee95af7
telafifi <telafifi@gmail.com>
2024-09-22 10:38:28 -0500

    Add commentary

--- Homework/homework2/homework/logger.py
+++ Homework/homework2/homework/logger.py
@@ -7,13 +7,12 @@ import torch.utils.tensorboard as tb
 
 def test_logging(logger: tb.SummaryWriter):
     """
-    Your code here - finish logging the dummy loss and accuracy
-
+    Here we will show how to log training and validation metrics to tensorboard
+    to visualize the output and allow for easier debugging and comparison of different runs
+    
     For training, log the training loss every iteration and the average accuracy every epoch
-    Call the loss 'train_loss' and accuracy 'train_accuracy'
 
     For validation, log only the average accuracy every epoch
-    Call the accuracy 'val_accuracy'
 
     Make sure the logging is in the correct spot so the global_step is set correctly,
     for epoch=0, iteration=0: global_step=0
@@ -21,6 +20,7 @@ def test_logging(logger: tb.SummaryWriter):
     # strongly simplified training loop
     global_step = 0
     for epoch in range(10):
+        # Hold the metrics to render the accuracies
         metrics = {"train_acc": [], "val_acc": []}
 
         # example training loop
@@ -29,15 +29,15 @@ def test_logging(logger: tb.SummaryWriter):
             dummy_train_loss = 0.9 ** (epoch + iteration / 20.0)
             dummy_train_accuracy = epoch / 10.0 + torch.randn(10)
 
-            # TODO: log train_loss
+            # log train_loss for each iteration
             logger.add_scalar('train_loss', dummy_train_loss, global_step)
 
-            # TODO: save additional metrics to be averaged
+            # save additional metrics to be averaged
             metrics["train_acc"].append(sum(dummy_train_accuracy) / len(dummy_train_accuracy))
 
             global_step += 1
 
-        # TODO: log average train_accuracy
+        # log average train_accuracy for each epoch
         avg_train_accuracy = sum(metrics["train_acc"]) / len(metrics["train_acc"])
         logger.add_scalar('train_accuracy', avg_train_accuracy, global_step)
 
@@ -46,10 +46,10 @@ def test_logging(logger: tb.SummaryWriter):
         for _ in range(10):
             dummy_validation_accuracy = epoch / 10.0 + torch.randn(10)
 
-            # TODO: save additional metrics to be averaged
+            # save additional metrics to be averaged
             metrics["val_acc"].append(sum(dummy_validation_accuracy) / len(dummy_validation_accuracy))
 
-        # TODO: log average val_accuracy
+        # log average val_accuracy for each epoch
         avg_val_accuracy = sum(metrics["val_acc"]) / len(metrics["val_acc"])
         logger.add_scalar('val_accuracy', avg_val_accuracy, global_step)
 
