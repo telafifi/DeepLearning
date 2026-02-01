c125347b6a3b2338b5f031aba911f145fe7ce0d9
Tarek <telafifi@gmail.com>
2026-01-31 23:30:29 -0600

    delete

--- Homework/homework1/homework/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import nearest_neighbor_classifier as nearest_neighbor_classifier
-from . import pytorch_basics as pytorch_basics
-from . import weather_forecast as weather_forecast
--- Homework/homework2/homework/__init__.py
+++
@@ -1,2 +0,0 @@
-from .logger import test_logging as test_logging
-from .models import load_model as load_model
